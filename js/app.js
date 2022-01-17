b20584780d5e102aafddf8bb90d618c6497d8757
efs0-cod3 <eflearnng@gmail.com>
2022-01-17 08:50:33 -0400

    lot of changes

--- js/app.js
+++ js/app.js
@@ -1,230 +1,139 @@
-let dBtn = document.getElementsByClassName('daily-btn')[0];
-let wBtn = document.getElementsByClassName('weekly-btn')[0];
-let mBtn = document.getElementsByClassName('monthly-btn')[0];
-let daily = document.getElementsByClassName('daily');
-let weekly = document.getElementsByClassName('weekly');
-let monthly = document.getElementsByClassName('monthly');
+const dBtn = document.getElementsByClassName('daily-btn')[0];
+const wBtn = document.getElementsByClassName('weekly-btn')[0];
+const mBtn = document.getElementsByClassName('monthly-btn')[0];
+const daily = document.getElementsByClassName('daily');
+const weekly = document.getElementsByClassName('weekly');
+const monthly = document.getElementsByClassName('monthly');
 
-let data = [{
-    "title": "Work",
-    "timeframes": {
-      "daily": {
-        "current": 5,
-        "previous": 7
-      },
-      "weekly": {
-        "current": 32,
-        "previous": 36
-      },
-      "monthly": {
-        "current": 103,
-        "previous": 128
-      }
-    }
-  },
-  {
-    "title": "Play",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 2
-      },
-      "weekly": {
-        "current": 10,
-        "previous": 8
-      },
-      "monthly": {
-        "current": 23,
-        "previous": 29
-      }
-    }
-  },
-  {
-    "title": "Study",
-    "timeframes": {
-      "daily": {
-        "current": 0,
-        "previous": 1
-      },
-      "weekly": {
-        "current": 4,
-        "previous": 7
-      },
-      "monthly": {
-        "current": 13,
-        "previous": 19
-      }
-    }
-  },
-  {
-    "title": "Exercise",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 1
-      },
-      "weekly": {
-        "current": 4,
-        "previous": 5
-      },
-      "monthly": {
-        "current": 11,
-        "previous": 18
-      }
-    }
-  },
-  {
-    "title": "Social",
-    "timeframes": {
-      "daily": {
-        "current": 1,
-        "previous": 3
-      },
-      "weekly": {
-        "current": 5,
-        "previous": 10
-      },
-      "monthly": {
-        "current": 21,
-        "previous": 23
-      }
-    }
-  },
-  {
-    "title": "Self-care",
-    "timeframes": {
-      "daily": {
-        "current": 0,
-        "previous": 1
-      },
-      "weekly": {
-        "current": 2,
-        "previous": 2
-      },
-      "monthly": {
-        "current": 7,
-        "previous": 11
-      }
-    }
-  }
-]
-
-let myData = JSON.parse(JSON.stringify(data))
-
+// set active to daily button and section
+let myData = []
 let stats = document.getElementsByClassName('stats')[0];
-myData.forEach((item) => {
-  let cards = document.createElement('div')
-  cards.classList.add('cont', `${item.title}`);
-  cards.innerHTML = `
-  <div class="cont__info stats__${item.title}">
+
+function getHours() {
+  fetch('js/data.json')
+  .then((res) => res.json())
+  .then((data) => {
+    myData = data;
+    let cards = '';
+        myData.forEach((data) => {
+          cards += `
+          <div class="cont ${data.title}">
+        <div class="cont__info stats__${data.title}">
    <div class="stats__title">
-      <h2 class="stats__title-name ${item.title}-title">${item.title}</h2>
+      <h2 class="stats__title-name ${data.title}-title">${data.title}</h2>
        <p>...</p>
      </div>
     <div class="stats__hours-section">
       <div class="daily daily--active">
          <p class="recent">
-         ${item.timeframes.daily.current}hrs
+         ${data.timeframes.daily.current}hrs
            <!-- daily -->
          </p>
          <p class="previous">
-           Yesterday - ${item.timeframes.daily.previous}hrs
+           Yesterday - ${data.timeframes.daily.previous}hrs
            <!-- daily -->
          </p>
        </div>
 
        <div class="weekly">
          <p class="recent">
-         ${item.timeframes.weekly.current}hrs
+         ${data.timeframes.weekly.current}hrs
           <!-- weekly -->
         </p>
         <p class="previous">
-           Last Week - ${item.timeframes.weekly.previous}hrs
+           Last Week - ${data.timeframes.weekly.previous}hrs
            <!-- weekly -->
          </p>
 
        </div>
        <div class="monthly">
          <p class="recent">
-         ${item.timeframes.monthly.current}hrs
+         ${data.timeframes.monthly.current}hrs
            <!-- monthly -->
          </p>
          <p class="previous">
-          Last Month - ${item.timeframes.monthly.previous}hrs
+          Last Month - ${data.timeframes.monthly.previous}hrs
           <!-- monthly -->
          </p>
       </div>
      </div>
   </div>
+ </div>
  </div>  
-  `
-  stats.append(cards)
-})
+          `
+          
+        });
+    
+    
+        stats.innerHTML = cards
+      })
+}
+getHours()
 
-// set active to daily button and section
-let showDaily = () => {
+
+const showDaily = () => {
   // button
   dBtn.classList.add('btn--active')
   // sections
-  for(let i = 0;i < daily.length; i++){
+  for (let i = 0; i < daily.length; i++) {
     daily[i].classList.add('daily--active');
   }
 }
 // when daily btn is active this remove active from the other buttons and sections
-let remove_wm = () => {
+const remove_wm = () => {
   wBtn.classList.remove('btn--active');
   mBtn.classList.remove('btn--active');
-  
-  for(let i = 0;i < weekly.length; i++){
+
+  for (let i = 0; i < weekly.length; i++) {
     weekly[i].classList.remove('weekly--active');
   }
-  for(let i = 0;i < monthly.length; i++){
+  for (let i = 0; i < monthly.length; i++) {
     monthly[i].classList.remove('monthly--active');
   }
 }
 // set active to weekly button and section
-let showWeekly = () => {
+const showWeekly = () => {
   // button
   wBtn.classList.add('btn--active');
   // sections
-  for(let i = 0;i < weekly.length; i++){
+  for (let i = 0; i < weekly.length; i++) {
     weekly[i].classList.add('weekly--active');
   }
 }
 
 // when weekly btn is active this remove active from the other buttons and sections
-let remove_dm = () => {
+const remove_dm = () => {
   dBtn.classList.remove('btn--active');
   mBtn.classList.remove('btn--active');
 
-  for(let i = 0;i < daily.length; i++){
+  for (let i = 0; i < daily.length; i++) {
     daily[i].classList.remove('daily--active');
   }
-  for(let i = 0;i < monthly.length; i++){
+  for (let i = 0; i < monthly.length; i++) {
     monthly[i].classList.remove('monthly--active');
   }
 }
 
 // set active to monthly button and section
-let showMonthly = () => {
+const showMonthly = () => {
   // button
   mBtn.classList.add('btn--active');
   // sections
-  for(let i = 0;i < monthly.length; i++){
+  for (let i = 0; i < monthly.length; i++) {
     monthly[i].classList.add('monthly--active');
   }
 }
 // when monthly btn is active this remove active from the other buttons and sections
-let remove_dw = () => {
+const remove_dw = () => {
   // btn
   dBtn.classList.remove('btn--active');
   wBtn.classList.remove('btn--active');
 
   // sections
-  for(let i = 0;i < daily.length; i++){
+  for (let i = 0; i < daily.length; i++) {
     daily[i].classList.remove('daily--active');
   }
-  for(let i = 0;i < weekly.length; i++){
+  for (let i = 0; i < weekly.length; i++) {
     weekly[i].classList.remove('weekly--active');
   }
 }
@@ -237,11 +146,8 @@ dBtn.addEventListener('click', () => {
 wBtn.addEventListener('click', () => {
   showWeekly()
   remove_dm()
-
 })
 mBtn.addEventListener('click', () => {
   showMonthly()
   remove_dw()
-
-
 })
